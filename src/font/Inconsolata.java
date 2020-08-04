c98aac5aa0e972c023a8a900485a44fd7f8ef784
Casey Pak <casemcpak@gmail.com>
2020-08-04 15:25:37 +0800

    Added Functions Panel and restructured program
    With custom made buttons :D
    Also made program unresizable so I can abuse null layout because all the other Swing layout is pure fucking cancer holy shit who the fuck designed those

---
+++ src/font/Inconsolata.java
@@ -0,0 +1,23 @@
+package userinterface.font;
+
+import java.awt.*;
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+
+public class Inconsolata {
+
+    public Font getFont(int size)
+    {
+        Font myfont = null;
+        Font myfontReal = null;
+
+        try{
+            InputStream is = new BufferedInputStream(this.getClass().getClassLoader().getResourceAsStream("userinterface/font/Inconsolata.ttf"));
+            myfont = Font.createFont(Font.TRUETYPE_FONT, is);
+            myfontReal = myfont.deriveFont(Font.PLAIN, size);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return myfontReal;
+    }
+}
---
+++ src/userinterface/components/Screenview.java
@@ -0,0 +1,4 @@
+package userinterface.components;
+
+public class Screenview {
+}
